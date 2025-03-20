aa6cc7103ee557e3847e3b4ccac1bbb50b7ae600
Mohammadreza Sadeghi <mohammadrezasadeqi161@gmail.com>
2025-03-20 22:13:55 +0200

    coming back to the last stable point (before timer)

--- QtApplication/headers/NetworkManager.h
+++ QtApplication/headers/NetworkManager.h
@@ -3,19 +3,16 @@
 
 #include <QNetworkAccessManager>
 #include <QString>
-#include <QObject>
 
 /**
  * NetworkManager class.
  * Handles network operations to fetch QML content from a specified server.
  */
-class NetworkManager : public QObject {
-    Q_OBJECT
+class NetworkManager{
 
 public:
     NetworkManager(); // Constructor
     QString fetchQML(const QString& filename); // Method to fetch QML content by filename.
-    Q_INVOKABLE void sendShutdownRequest();  // Method to send a shutdown request to the server
 
 private:
     QNetworkAccessManager manager; // Network access manager to handle requests.
--- QtApplication/source/main.cpp
+++ QtApplication/source/main.cpp
@@ -1,27 +1,14 @@
 #include "NetworkManager.h"
-#include "ApplicationManager.h"
-#include "TimerManager.h"
 #include <QGuiApplication>
 #include <QQmlApplicationEngine>
-#include <QUrl>
-#include <QDir>
 #include <QDebug>
-#include <QNetworkAccessManager>
-#include <QNetworkRequest>
-#include <QNetworkReply>
-#include <QJsonDocument>
-#include <QJsonObject>
-#include <QEventLoop>
 
 int main(int argc, char *argv[])
 {
     QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
     QGuiApplication app(argc, argv);
 
-    // new
-    qmlRegisterType<TimerManager>("com.yourcompany.timer", 1, 0, "TimerManager");
-    qmlRegisterType<ApplicationManager>("com.yourcompany.system", 1, 0, "ApplicationManager");
-    qmlRegisterType<NetworkManager>("com.yourcompany.network", 1, 0, "NetworkManager");
+    
 
     NetworkManager networkManager; // Create an instance of NetworkManager to handle network operations.
     QString qmlContent = networkManager.fetchQML("Main.qml");
