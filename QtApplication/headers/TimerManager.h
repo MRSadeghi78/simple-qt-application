8a2c0fbad2d7da2459a4762e5f7f94eeb75a5e85
Mohammadreza Sadeghi <mohammadrezasadeqi161@gmail.com>
2025-03-21 00:03:30 +0200

    trying to fix timer errors

--- QtApplication/headers/TimerManager.h
+++ QtApplication/headers/TimerManager.h
@@ -8,7 +8,7 @@ class TimerManager : public QObject {
     Q_OBJECT  // Make sure this macro is present and correctly placed
 public:
     explicit TimerManager(QObject *parent = nullptr);
-    virtual ~TimerManager();  // Ensure virtual destructor is declared if needed
+    virtual ~TimerManager() = 0;  // Ensure virtual destructor is declared if needed
 
     Q_INVOKABLE void startTimer();
     Q_INVOKABLE void stopTimer();
